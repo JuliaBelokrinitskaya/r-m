5e64c60fd0a97c7f93c8e941a0f9befe443338d0
Julia Belokrinitskaya <bel_julia@mail.ru>
2019-10-31 15:58:15 +0300

    add styles and randomness

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,8 @@ const getCharacter = ({name, image, location}) => ({
   name,
   image,
   location: location.name,
-  value: 1
+  value: (Math.random() + 1) * 2,
+  rotate: (Math.random() - 0.5) * 40
 });
 
 function getHierarchy(items, property) {
@@ -21,13 +22,14 @@ function getHierarchy(items, property) {
   d3.group(items, item => item[property]).forEach((val, key) => {
     data.children.push({
       name: key,
+      rotate: (Math.random() - 0.5) * 40,
       children: val
     });
   });
 
   return d3.hierarchy(data)
     .sum(item => item.value)
-    .sort((item1, item2) => item2.value - item1.value);
+    .sort((item1, item2) => Math.random());
 }
 
 function pack(items, property) {
@@ -61,59 +63,64 @@ const getAllCharacters = (url) => {
         const root = pack(characters, `location`);
         let focus = root;
         let view;
+        let hue = Math.floor(Math.random() * 80) + 180;
 
         const color = d3.scaleLinear()
-          .domain([0, 5])
-          .range([`hsl(152,80%,80%)`, `hsl(228,30%,40%)`])
+          .domain([0, 20])
+          .range([`hsl(${(hue + 30) % 360},50%,40%)`, `hsl(${(hue - 30) % 360},70%,70%)`])
           .interpolate(d3.interpolateHcl)
 
         const svg = d3.create(`svg`)
           .classed(`circle-pack`, true)
           .attr(`viewBox`, `-${width / 2} -${height / 2} ${width} ${height}`)
+          .style(`background-color`, `hsl(${hue}, 60%, 10%)`)
           .on(`click`, () => zoom(root));
 
         const box = svg.append(`g`);
 
         const groups = box
+          .attr(`pointer-events`, `none`)
           .selectAll(`g`)
           .data(root.descendants().slice(1))
-          .join(`g`)
-            .attr(`pointer-events`, d => !d.children ? `none` : null)
-            .on(`mouseover`, function() { d3.select(this).attr(`stroke`, `#ffffff`); })
-            .on(`mouseout`, function() { d3.select(this).attr(`stroke`, null); })
-            .on(`click`, d => focus !== d && (zoom(d), d3.event.stopPropagation()));
+          .join(`g`);
 
         const circles = groups.filter(d => !d.data.id)
           .append(`circle`)
-          .attr(`fill`, d => color(d.depth + Math.random()))
+          .attr(`fill`, d => color(Math.random() * 20));
 
         const clipPaths = groups.filter(d => d.data.id)
           .append(`clipPath`)
             .attr(`id`, d => `circle-${d.data.id}`)
-            .append(`circle`)
+            .append(`circle`);
 
         const images = groups.filter(d => d.data.id)
           .append(`image`)
             .attr(`clip-path`, d => `url(#circle-${d.data.id})`)
-            .attr(`xlink:href`, d => d.data.image)
+            .attr(`xlink:href`, d => d.data.image);
 
         const label = svg.append(`g`)
-          .style(`font`, `10px sans-serif`)
-          .attr(`pointer-events`, `none`)
-          .attr(`text-anchor`, `middle`)
-          .selectAll(`text`)
+          .selectAll(`foreignObject`)
           .data(root.descendants())
-          .join(`text`)
-            .style(`fill-opacity`, d => d.parent === root ? 1 : 0)
-            .style(`display`, d => d.parent === root ? `inline` : `none`)
-            .text(d => d.data.name);
+          .join(`foreignObject`)
+            .style(`overflow`, `visible`)
+            .classed(`hidden`, d => d.parent !== root)
+            .on(`click`, d => focus !== d && (zoom(d), d3.event.stopPropagation()));
+
+        const labelText = label
+          .append(`xhtml:div`)
+          .classed(`circle-pack__label`, true)
+            .append(`p`)
+              .style(`transform`, d => `rotate(${d.data.rotate}deg)`)
+              .text(d => d.data.name);
 
         zoomTo([root.x, root.y, root.r * 2]);
 
         function zoomTo(v) {
           const k = Math.min(width, height) / v[2];
           view = v;
-          label.attr(`transform`, d => `translate(${(d.x - v[0]) * k},${(d.y - v[1] + (d.data.id ? d.r / 2 : 0)) * k})`);
+          label.attr(`transform`, d => `translate(${(d.x - v[0] - d.r) * k},${(d.y - v[1] - d.r) * k})`);
+          label.attr(`width`, d => d.r * k * 2);
+          label.attr(`height`, d => d.r * k * 2);
           groups.attr(`transform`, d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
           circles.attr(`r`, d => d.r * k);
           clipPaths.attr(`r`, d => d.r * k);
@@ -136,14 +143,16 @@ const getAllCharacters = (url) => {
               });
 
           label
+            .classed(`hidden`, d => d.parent !== focus)
             .filter(function(d) { return d.parent === focus || this.style.display === `inline`; })
-            .transition(transition)
-              .style(`fill-opacity`, d => d.parent === focus ? 1 : 0)
-              .on(`start`, function(d) { if (d.parent === focus) this.style.display = `inline`; })
-              .on(`end`, function(d) { if (d.parent !== focus) this.style.display = `none`; });
+            .transition(transition);
         }
 
         document.body.appendChild(svg.node());
+
+        window.setTimeout(function () {
+          zoom(root);
+        }, 1000);
 
         window.addEventListener(`resize`, () => {
           width = window.innerWidth;
